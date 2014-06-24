94bd8a58c0257d5fe725fa2919eccd5938c56367
thekot <rereflex@gmail.com>
2014-06-24 15:37:32 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,13 @@ wget --no-verbose --no-check-certificate https://raw.github.com/thekot/dotfiles/
 [ -f ~/.screenrc ] && cp --backup=numbered ~/.screenrc ~/.screenrc.original
 wget --no-verbose --no-check-certificate https://raw.github.com/thekot/dotfiles/master/.screenrc -O ~/.screenrc
 
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/colors
+# ~/.vim directory structure
+mkdir -p ~/.vim/{swap,backup,colors,ftdetect,indent,syntax}
+
+# vim jellybeans colors theme
 wget --no-verbose --no-check-certificate https://raw.github.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim -O ~/.vim/colors/jellybeans.vim
+
+# vim nginx syntax files
+wget --no-verbose http://hg.nginx.org/nginx/raw-file/tip/contrib/vim/ftdetect/nginx.vim -O ~/.vim/ftdetect/nginx.vim
+wget --no-verbose http://hg.nginx.org/nginx/raw-file/tip/contrib/vim/indent/nginx.vim -O ~/.vim/indent/nginx.vim
+wget --no-verbose http://hg.nginx.org/nginx/raw-file/tip/contrib/vim/syntax/nginx.vim -O ~/.vim/syntax/nginx.vim
