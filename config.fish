bdf7bfff4e53baa5b03b090a2af0007ef4142785
Kot <1192090+koteq@users.noreply.github.com>
2018-12-13 17:50:15 +0300

    Add locale settings

--- config.fish
+++ config.fish
@@ -8,6 +8,12 @@ set -g theme_display_git no
 # Add home bin
 set PATH $PATH ~/bin/
 
+# Set language to English but with Russian locale
+set -x LANG en_US.UTF-8
+set -x LC_TIME ru_RU.UTF-8
+set -x LC_NUMERIC ru_RU.UTF-8
+set -x LC_MONETARY ru_RU.UTF-8
+
 # Ls for humans
 alias ls 'ls --color=auto --almost-all --classify --group-directories-first -v'
 
